bc82b25de6056d8a4c7af2c13e704864ff9c6bbd
NadetDev <nadetdev@gmail.com>
2025-03-08 22:44:22 +0100

    dummy test structure

---
+++ src/test/java/stepDefinitions/MainSteps.java
@@ -0,0 +1,27 @@
+package stepDefinitions;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class MainSteps {
+    @Given("User is on netBanking landing page")
+    public void user_is_on_net_banking_landing_page() {
+       System.out.println("User landed on netBanking landing page");
+    }
+
+    @When("User login into application")
+    public void user_login_into_application() {
+      System.out.println("User logged into application");
+    }
+
+    @Then("Home Page is displayed")
+    public void home_page_is_displayed() {
+      System.out.println("Home Page is displayed");
+    }
+
+    @Then("Cards are displayed")
+    public void cards_are_displayed() {
+       System.out.println("Cards are displayed");
+    }
+}
